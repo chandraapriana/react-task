3ecb727be6c1027112391e054ea06920085e1d36
Chandra Muhamad Apriana <50396186+chandraapriana@users.noreply.github.com>
2020-09-05 01:28:47 +0700

    mengupdate app.js dan app.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Tugas9 from "./Tugas-9/tugas9.js";
+import Tugas10 from "./Tugas-10/tugas10.js";
 
 function App() {
   return (
     <div className="App">
-      <div class="container">
-        <h1>Form Pembelian Buah</h1>
-        <table>
-          <tr>
-            <td>
-              <b>Nama Pelanggan</b>
-            </td>
-            <td>
-              <input type="text" />
-            </td>
-          </tr>
-          <tr>
-            <td class="text-bottom">
-              <b>Daftar Item</b>
-            </td>
-            <td>
-              <input type="checkbox" /> Semangka <br />
-              <input type="checkbox" /> Jeruk <br />
-              <input type="checkbox" /> Nanas <br />
-              <input type="checkbox" /> Salak <br />
-              <input type="checkbox" /> Anggur <br />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button>Kirim</button>
-            </td>
-          </tr>
-        </table>
-      </div>
+      <Tugas9 />
+      <Tugas10 />
     </div>
   );
 }
